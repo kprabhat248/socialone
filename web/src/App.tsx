427ee584dc7c85c420dca5ba0baa0aac522c6dec
Prabhat Kumar <prabhatkumar@Prabhats-MacBook-Air.local>
2025-03-19 16:09:12 +0530

    frontend react

---
+++ web/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/v1"
+function App() {
+
+
+  return (
+    <>
+      <div>App Home Screen</div>
+      </>
+  )
+}
+
+
+export default App
